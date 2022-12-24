b8c3d353ce01cb35bdadd29fd5f4c870f7f01a4e
Vlad <vladot@ya.ru>
2022-12-24 21:07:38 +0600

    Coursework1

--- src/pro/sky/java/course1/coursework1/Employee.java
+++ src/pro/sky/java/course1/coursework1/Employee.java
@@ -1,4 +1,51 @@
 package pro.sky.java.course1.coursework1;
 
 public class Employee {
+
+    private String employeeFio;
+    private int employeeDepartment;
+    private double employeeSalary;
+    private final int id;
+    private static int counter;
+
+    public Employee(String fio, int department, double salary) {
+        this.employeeFio = fio;
+        this.employeeDepartment = department;
+        this.employeeSalary = salary;
+        id = counter;
+        counter++;
+    }
+
+    public String getEmployeeFio() {
+        return this.employeeFio;
+    }
+
+    public int getEmployeeDepartment() {
+        return  this.employeeDepartment;
+    }
+
+    public double getEmployeeSalary() {
+        return this.employeeSalary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String toString() {
+        return "ID: " + id + "; ФИО: " + employeeFio + "; Отдел: " + employeeDepartment + "; Оклад: " + employeeSalary;
+    }
+
+    public void setEmployeeFio(String employeeFio) {
+        this.employeeFio = employeeFio;
+    }
+
+    public void setEmployeeDepartment(int employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public void setEmployeeSalary(double employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
 }
+
--- src/pro/sky/java/course1/coursework1/EmployeesBase.java
+++ src/pro/sky/java/course1/coursework1/EmployeesBase.java
@@ -1,4 +1,90 @@
 package pro.sky.java.course1.coursework1;
 
+import java.util.Arrays;
+
 public class EmployeesBase {
+
+    static int size = 10;
+    static Employee[] employees = new Employee[size];
+
+
+    void addEmployee(Employee employee) {
+        int index = -1;
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index < 0) {
+            System.out.println("В справочнике нет места.");
+        }
+
+        employees[index] = employee;
+        System.out.println("Новый сотрудник " + employee.getEmployeeFio() + " добавлен.");
+    }
+
+    static void printEployeesList() {
+        System.out.println(Arrays.toString(employees));
+    }
+
+    static double calculateSalarySum() {
+        double salarySum = 0;
+        for (Employee employee : employees) {
+            salarySum = salarySum + employee.getEmployeeSalary();
+        }
+        return salarySum;
+    }
+
+    static String findMinSalary() {
+
+        String minSalaryEmployee = null;
+        int index;
+        double minSalary = employees[0].getEmployeeSalary();
+        for (int i = 1; i < employees.length; i++) {
+            if (minSalary > employees[i].getEmployeeSalary()) {
+
+                minSalary = employees[i].getEmployeeSalary();
+                index = i;
+                minSalaryEmployee = employees[index].getEmployeeFio();
+            }
+        }
+        return "Сотрудник " + minSalaryEmployee + " имеет минимальную ЗП (" + minSalary + ")";
+    }
+
+    static String findMaxSalary() {
+        String maxSalaryEmployee = null;
+        int index;
+        double maxSalary = employees[0].getEmployeeSalary();
+        for (int i = 1; i < employees.length; i++) {
+            if (maxSalary < employees[i].getEmployeeSalary()) {
+                maxSalary = employees[i].getEmployeeSalary();
+                index = i;
+                maxSalaryEmployee = employees[index].getEmployeeFio();
+            }
+        }
+        return "Сотрудник " + maxSalaryEmployee + " имеет максимальную ЗП (" + maxSalary + ")";
+    }
+
+
+    static double calculateAverageSalaryValue() {
+        double averageSalaryValue;
+        averageSalaryValue = calculateSalarySum() / size;
+        return averageSalaryValue;
+    }
+
+    static void printEmployeeFio() {
+        for (Employee employee : employees) {
+            System.out.println(employee.getEmployeeFio());
+        }
+    }
 }
+
+
+
+
+
+
+
